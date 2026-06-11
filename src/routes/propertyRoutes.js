d2f5f4d0167322eba7cc6ec986bb7a0ae83bc44c
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2026-06-11 16:29:13 +0530

    initial propertyreviwew change

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProperties, getProperty } = require('../controllers/propertycontroller');
+const { getProperties, getProperty } = require('../controllers/propertyController');
 
 router.get('/', getProperties);
 router.get('/:id', getProperty);
